28635f632df22360f5d9f6321962c1377109f372
pcausey <71686859+pcausey@users.noreply.github.com>
2022-09-18 16:13:12 -0600

    MAKE IT PRETTY

--- L1C_Conversion_Pipeline/variables.py
+++ L1C_Conversion_Pipeline/variables.py
@@ -3,3 +3,12 @@ UNITS = 'units'
 DATA = 'data'
 FILL = 'fill'
 SCALE = 'scale'
+
+OBSERVATION_DATA = 'observation_data'
+GEOLOCATION_DATA = 'geolocation_data'
+BIN_ATTRIBUTES = 'bin_attributes'
+
+Q = 'Q'
+U = 'U'
+I_P = 'I_P'
+I_NP = 'I_NP'
