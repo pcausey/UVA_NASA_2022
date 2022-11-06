c3d6b6226c83bb96835bc42da97d69f02ea3725d
pcausey <71686859+pcausey@users.noreply.github.com>
2022-11-06 08:45:39 -0700

    kdtree upgrade

--- L1C_Conversion_Pipeline/Grasp_Pipeline/grasp_file.py
+++ L1C_Conversion_Pipeline/Grasp_Pipeline/grasp_file.py
@@ -3,7 +3,7 @@ import pandas as pd
 from L1C_Conversion_Pipeline.hdf_file_class import HDFFile, HDFColumn
 from L1C_Conversion_Pipeline.Grasp_Pipeline.grasp_dict import grasp_list, GRASP_LATITUDE, GRASP_LONGITUDE
 from L1C_Conversion_Pipeline.variables import *
-from copy import deepcopy
+from scipy.spatial import KDTree
 
 
 class GraspFile(HDFFile):
@@ -105,14 +105,31 @@ class GraspFile(HDFFile):
 
         lats = caltrack.final_dict['geolocation_data']['latitude']['data'][0]
         lons = caltrack.final_dict['geolocation_data']['longitude']['data'][0]
+        caltrack_lat_lon = np.stack([lats, lons], axis=1)
 
         grasp_df = self.build_grasp_df()
 
-        grasp_matcher = GraspMatcher(grasp_df, self.fill_value)
-        function = grasp_matcher.match_single_lat_lon
+        # _, closest_1_idx = epa_kdtree.query(caltrack_lat_lon, 1)
+        # closest_1_latlon = self.epa_data[closest_1_idx]
 
-        matching_list_of_dfs = map(function, lats, lons)
-        matching_list_of_dfs = list(matching_list_of_dfs)
+        grasp_lat_lon = np.stack([grasp_df[GRASP_LATITUDE].to_numpy(),
+                                  grasp_df[GRASP_LONGITUDE].to_numpy()], axis=1)
+        grasp_kdtree = KDTree(grasp_lat_lon)
+
+        idx = grasp_kdtree.query_ball_point(caltrack_lat_lon, r=0.1)
+
+        matching_list_of_dfs = []
+        for x in idx:
+            if len(x) > 0:
+                matching_list_of_dfs.append(grasp_df.iloc[[x[0]]])
+            else:
+                matching_list_of_dfs.append(self.build_empty_dataframe(grasp_df, self.fill_value))
+
+        # grasp_matcher = GraspMatcher(grasp_df, self.fill_value)
+        # function = grasp_matcher.match_single_lat_lon
+        #
+        # matching_list_of_dfs = map(function, lats, lons)
+        # matching_list_of_dfs = list(matching_list_of_dfs)
 
         full_matching_df = pd.concat(matching_list_of_dfs)
 
@@ -125,42 +142,6 @@ class GraspFile(HDFFile):
                 UNITS: self.final_dict[col][UNITS]
             }
 
-
-class GraspMatcher:
-    def __init__(self, grasp_df, fill_value):
-        self.grasp_df = grasp_df
-        self.fill_value = fill_value
-
-    def match_single_lat_lon(self, d1_lat, d1_lon):
-
-        grasp_data = self.filter_grasp_data(d1_lat, d1_lon)
-
-        df = self.run_grasp_data_lookup(d1_lat, d1_lon, grasp_data)
-
-        return df
-
-    def filter_grasp_data(self, d1_lat, d1_lon):
-        # grasp_data = deepcopy(self.grasp_df)
-        degree_search = 0.1
-
-        # Limit search to 1 degree in all directions
-        lat_mask = ((self.grasp_df[GRASP_LATITUDE] >= d1_lat - degree_search)
-                    & (self.grasp_df[GRASP_LATITUDE] <= d1_lat + degree_search))
-        lon_mask = ((self.grasp_df[GRASP_LONGITUDE] >= d1_lon - degree_search)
-                    & (self.grasp_df[GRASP_LONGITUDE] <= d1_lon + degree_search))
-
-        grasp_data = self.grasp_df[lat_mask & lon_mask]
-
-        # Look for where other cols aren't equal to fill value (we'll just default to fill value if it is)
-        cols = [col for col in grasp_data.columns]
-        cols.remove(GRASP_LONGITUDE)
-        cols.remove(GRASP_LATITUDE)
-
-        query = ' & '.join([f'{col} != {self.fill_value}' for col in cols])
-        grasp_data = grasp_data.query(query)
-
-        return grasp_data
-
     @staticmethod
     def build_empty_dataframe(df, fill_value):
         dict_copy = {}
@@ -170,30 +151,66 @@ class GraspMatcher:
         df = pd.DataFrame(dict_copy, index=[0])
         return df
 
-    def run_grasp_data_lookup(self, d1_lat, d1_lon, grasp_data):
-        from L1C_Conversion_Pipeline.Grasp_Pipeline.grasp_dict import GRASP_LATITUDE, GRASP_LONGITUDE
-        from L1C_Conversion_Pipeline.distance_functions import calc_spherical_distance
 
-        try:
-
-            if len(grasp_data) == 0:
-                # if len(grasp_data) == 0:
-                #     # Return a dataframe with all rows filled with Fill Value
-                return self.build_empty_dataframe(grasp_data, self.fill_value)
-
-            # Run below if Lat/Lon/PM25 filter > 0 OR Lat/Lon filter > 0
-            dist, index_min, min_lat, min_lon = \
-                calc_spherical_distance(
-                    d1_lat, d1_lon,
-                    grasp_data[GRASP_LATITUDE], grasp_data[GRASP_LONGITUDE],
-                    verbose=False
-                )
-
-            data = grasp_data.iloc[[index_min]]
-
-            # Return a Row
-            return data
-
-        except Exception as e:
-            print(f'Grasp Lookup: error on lat: {d1_lat} and lon: {d1_lon}')
-            return self.build_empty_dataframe(grasp_data, self.fill_value)
+# class GraspMatcher:
+#     def __init__(self, grasp_df, fill_value):
+#         self.grasp_df = grasp_df
+#         self.fill_value = fill_value
+#
+#     def match_single_lat_lon(self, d1_lat, d1_lon):
+#
+#         grasp_data = self.filter_grasp_data(d1_lat, d1_lon)
+#
+#         df = self.run_grasp_data_lookup(d1_lat, d1_lon, grasp_data)
+#
+#         return df
+#
+#     def filter_grasp_data(self, d1_lat, d1_lon):
+#         # grasp_data = deepcopy(self.grasp_df)
+#         degree_search = 0.1
+#
+#         # Limit search to 1 degree in all directions
+#         lat_mask = ((self.grasp_df[GRASP_LATITUDE] >= d1_lat - degree_search)
+#                     & (self.grasp_df[GRASP_LATITUDE] <= d1_lat + degree_search))
+#         lon_mask = ((self.grasp_df[GRASP_LONGITUDE] >= d1_lon - degree_search)
+#                     & (self.grasp_df[GRASP_LONGITUDE] <= d1_lon + degree_search))
+#
+#         grasp_data = self.grasp_df[lat_mask & lon_mask]
+#
+#         # Look for where other cols aren't equal to fill value (we'll just default to fill value if it is)
+#         cols = [col for col in grasp_data.columns]
+#         cols.remove(GRASP_LONGITUDE)
+#         cols.remove(GRASP_LATITUDE)
+#
+#         query = ' & '.join([f'{col} != {self.fill_value}' for col in cols])
+#         grasp_data = grasp_data.query(query)
+#
+#         return grasp_data
+#
+#     def run_grasp_data_lookup(self, d1_lat, d1_lon, grasp_data):
+#         from L1C_Conversion_Pipeline.Grasp_Pipeline.grasp_dict import GRASP_LATITUDE, GRASP_LONGITUDE
+#         from L1C_Conversion_Pipeline.distance_functions import calc_spherical_distance
+#
+#         try:
+#
+#             if len(grasp_data) == 0:
+#                 # if len(grasp_data) == 0:
+#                 #     # Return a dataframe with all rows filled with Fill Value
+#                 return self.build_empty_dataframe(grasp_data, self.fill_value)
+#
+#             # Run below if Lat/Lon/PM25 filter > 0 OR Lat/Lon filter > 0
+#             dist, index_min, min_lat, min_lon = \
+#                 calc_spherical_distance(
+#                     d1_lat, d1_lon,
+#                     grasp_data[GRASP_LATITUDE], grasp_data[GRASP_LONGITUDE],
+#                     verbose=False
+#                 )
+#
+#             data = grasp_data.iloc[[index_min]]
+#
+#             # Return a Row
+#             return data
+#
+#         except Exception as e:
+#             print(f'Grasp Lookup: error on lat: {d1_lat} and lon: {d1_lon}')
+#             return self.build_empty_dataframe(grasp_data, self.fill_value)
